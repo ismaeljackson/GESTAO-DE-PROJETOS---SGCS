1ba5613f9dff7bcc5d8c693ee639825ed61f816c
ismaeljackson <ismael.j.b.silveira@gmail.com>
2026-09-23 19:48:29 -0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,3 +0,0 @@
-// Configurações Globais do SGCS
-export const SUPABASE_URL = 'https://rjafhrszdkjmuztpuzux.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_GqgBAZ3EA-uER8CDaqDTBg_Z4bkq3XV';
